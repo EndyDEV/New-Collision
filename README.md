c23a03c664aff5518e4247487b3de444b66fdc66
EndyDEV <75427605+EndyDEV@users.noreply.github.com>
2020-12-08 15:50:54 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# New-Collision
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,33 @@
+var mr,fr
+
+function setup() {
+  createCanvas(800,400);
+  mr = createSprite(600, 200, 50, 50);
+  fr = createSprite(400,300,50,50);
+mr.shapeColor = "red"
+fr.shapeColor = "blue"
+
+}
+
+function draw() {
+  background("black");  
+
+  mr.x = World.mouseX
+  mr.y = World.mouseY
+
+  if(mr.x-fr.x < mr.width/2 + fr.width/2 && fr.x-mr.x < mr.width/2 + fr.width/2 
+    && mr.y-fr.y < mr.height/2 + fr.height/2 && fr.y-mr.y < mr.height/2 + fr.height/2){
+
+mr.shapeColor = "yellow"
+fr.shapeColor = "yellow"
+    }
+
+    else{
+mr.shapeColor = "red"
+fr.shapeColor = "blue"
+
+    }
+    
+
+  drawSprites();
+}
